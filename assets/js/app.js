76b975eb2f355efa7bce469b45f928d0bf610f11
eutagocode <thiagoluix21@gmail.com>
2024-11-03 19:00:58 -0300

    feat: :sparkles: Introdução dos feriados nos dias faltando
    Foram introduzidas as datas dos feriados para não contar nos dias faltando

--- assets/js/app.js
+++ assets/js/app.js
@@ -120,12 +120,30 @@ function proportionalWorkingDays(year, month, currentDay, goal) {
 function getRemainingBusinessDays(year, month, currentDay) {
     const today = new Date(year, month, currentDay);
     let lastDay = new Date(year, month + 1, 0);
+    const nationalHolidays = [
+        new Date(year, "0", "1"),
+        new Date(year, "1", "12"),
+        new Date(year, "1", "13"),
+        new Date(year, "2", "29"),
+        new Date(year, "3", "21"),
+        new Date(year, "4", "1"),
+        new Date(year, "4", "30"),
+        new Date(year, "8", "7"),
+        new Date(year, "9", "12"),
+        new Date(year, "10", "2"),
+        new Date(year, "10", "15"),
+        new Date(year, "11", "25"),
+    ];
 
     let businessDaysRemaining = [];
 
     for (let day = today; day <= lastDay; day.setDate(day.getDate() + 1)) {
         let dayOfTheWeek = day.getDay();
-        if (dayOfTheWeek !== 0) {
+        let isHoliday = nationalHolidays.some(
+            (holiday) => holiday.getTime() === day.getTime(),
+        );
+
+        if (dayOfTheWeek !== 0 && !isHoliday) {
             businessDaysRemaining.push(new Date(day));
         }
     }
@@ -181,8 +199,6 @@ function handleSoldValue() {
     data.sold = parseFloat(soldValue);
 
     localStorage.setItem("DATA_OF_GOAL", JSON.stringify(data));
-
-    updateDaily();
 
     fetchGoal();
 
