a073cd558aaba651f94f80f99d6a543ae71d93d2
eutagocode <thiagoluix21@gmail.com>
2024-12-28 17:29:56 -0300

    feat: :sparkles: Foi feita a instalação e configuração do tailwindCSS no projeto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        fontFamily: {
+            figtree: ["Figtree", "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
